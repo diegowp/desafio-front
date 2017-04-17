16fecb4e31d3b6cedf9d8a125a83a9a236c892de
Diego Teixeira <diego.teixeira@cscdoesporte.com.br>
2017-04-17 17:56:02 -0300

    just a commit

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 var gulp      = require('gulp');
 var connect   = require('gulp-connect');
 var sass      = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('connect', function() {
     // content
@@ -15,8 +16,10 @@ gulp.task('connect', function() {
 gulp.task('sass', function() {
     // content
     return gulp.src('./scss/*.scss')
-      .pipe(sass().on('error',sass.logError))
-      //.pipe(sass({outputStyle:'compressed'}))
+      .pipe(sourcemaps.init())
+      .pipe(sass().on('error', sass.logError))
+      .pipe(sass({outputStyle:'compressed'}))
+      .pipe(sourcemaps.write('./'))
       .pipe(gulp.dest('./css'))
       .pipe(connect.reload());
 });
